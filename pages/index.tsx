2f1c9d7a7c20425d3a128b4fafb9d426415076b2
James Laugerman <jameslaugerman@gmail.com>
2022-01-19 23:18:37 -0600

    everyone is steph

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from "next";
 import PlayerCard from "./playerCard";
+import styles from "../styles/Home.module.css";
+import Header from "./header";
+import Footer from "./footer";
 
 const Home: NextPage = () => {
-	return <PlayerCard />;
+	return (
+		<div className={styles.container}>
+			<Header />
+			<main className={styles.main}>
+				<div className={styles.grid}>
+					<PlayerCard playerId={115} name={"Steph Curry"} />;
+					<PlayerCard playerId={125} name={"Demar DeRozan"} />;
+					<PlayerCard playerId={115} />;
+				</div>
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
