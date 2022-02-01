2d69d554c61e5551b7cfcb9f7c5b8a7252d0730d
James Laugerman <jameslaugerman@gmail.com>
2022-01-31 20:16:10 -0600

    working useEffect

--- src/Components/PlayerCard.js
+++ src/Components/PlayerCard.js
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from "react";
 import styles from "./PlayerCard.module.css";
 
-export default function PlayerCard(props) {
+export default function PlayerCard({ id, img, player }) {
 	const [post, getPost] = useState([]);
 
-	const API = `https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${props.id}`;
-
-	const fetchPost = () => {
-		fetch(API)
-			.then((res) => res.json())
-			.then((res) => {
-				const playerData = res.data;
-				console.log(playerData);
-				getPost(playerData);
-			});
-	};
-
 	useEffect(() => {
+		const API = `https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${id}`;
+
+		const fetchPost = () => {
+			fetch(API)
+				.then((res) => res.json())
+				.then((res) => {
+					const playerData = res.data;
+					console.log(playerData);
+					getPost(playerData);
+				});
+		};
 		fetchPost();
-	}, []);
+	}, [id]);
 
 	return (
 		<div className={styles.card}>
-			<h2>{props.player}</h2>
+			<h2>{player}</h2>
 			<div>
 				{post.map((player, i) => {
 					return (
 						<div key={i}>
-							<img src={props.img} alt="Credit - theringer.com"></img>
+							<img src={img} alt="Credit - theringer.com"></img>
 							<p>Games Played - {player.games_played}</p>
 							<p>PPG - {player.pts}</p>
 							<p>RPG - {player.reb}</p>
